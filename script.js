cedfd8671a3fcef381f05efb4290e5471e0d0c30
robert-er <robert.roza.pl@gmail.com>
2020-09-29 02:49:23 +0200

    http instead of https

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://rocky-sands-77117.herokuapp.com/v1/task/';
+  var apiRoot = 'http://rocky-sands-77117.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
